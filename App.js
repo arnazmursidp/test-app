8b38dd4b289be747521eb9ce14141893f0795486
Arnaz <arnazmursid@gmail.com>
2017-11-30 10:26:19 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import reducer from './src/reducers';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Text>Test</Text>
+    );
+  }
+}
