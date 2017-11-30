5c8633b2f20b172239bf6cbfae3809e1aff62a10
Arnaz <arnazmursid@gmail.com>
2017-11-30 18:56:27 +0700

    remove unused components

--- src/index.js
+++ src/index.js
@@ -13,31 +13,3 @@ export default class Page extends React.Component {
     );
   }
 }
-
-// import React from 'react';
-// import { FlatList, View, StyleSheet } from 'react-native';
-// import { connect } from 'react-redux';
-
-// class ListTodo extends React.Component {
-//   render() {
-//     return (
-//         <View style={styles.container}>
-//             {this.props.todo.map((data, index) => {
-//                 console.log("Data", data);
-//             })}
-//         </View>  
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1
-//     }
-// });
-
-// const mapStateToProps = ({ todos }) => {
-//     todo: todos
-// }
-
-// export default connect(mapStateToProps, null)(ListTodo);
