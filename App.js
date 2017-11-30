995767494a5280ed8d4493e82f607c54758fea2e
Arnaz <arnazmursid@gmail.com>
2017-11-30 12:21:38 +0700

    fix reducer path

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import reducer from './src/reducers';
+import reducer from './src/reducer/reducers';
 import Page from './src/index';
 
 export default class App extends React.Component {
