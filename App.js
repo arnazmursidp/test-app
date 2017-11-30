582ac64f7e6b100f98dec2bfdefd1d176276e195
Arnaz <arnazmursid@gmail.com>
2017-11-30 17:40:59 +0700

    single-ing file store and reducer

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import Page from './src/index';
+import todos from './src/reducersss/todos';
+
+const reducer = combineReducers({
+  todos
+});
+
+const store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <Page />
+        <Provider store={store}>
+            <Page />
+        </Provider>    
     );
   }
 }
