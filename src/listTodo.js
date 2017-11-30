fb308645819dd7ff6c76b0d8278075e97ad517ed
Arnaz <arnazmursid@gmail.com>
2017-11-30 14:43:55 +0700

    connect the list todo

--- src/listTodo.js
+++ src/listTodo.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { ListView, View } from 'react-native';
+import { FlatList, View } from 'react-native';
+import { connect } from 'react-redux';
 
-export default class AddTodo extends React.Component {
+class ListTodo extends React.Component {
   render() {
     return (
         <View style={styles.container}>
-            
+            {todos.map((data, index) => {
+                console.log("Data");
+            })}
         </View>  
     );
   }
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: '1'
+    }
 });
+
+const stateToProps = (state) => {
+    todos: state.todos
+}
+
+export default connect(stateToProps)(ListTodo)
