b78b6db54a5d7517b3078824a42c807a9ed620be
Arnaz <arnazmursid@gmail.com>
2017-11-30 12:21:57 +0700

    import addtodo component

--- src/index.js
+++ src/index.js
@@ -1,37 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import AddTodo from './addTodo';
 
 export default class Page extends React.Component {
   render() {
     return (
-        <View style={styles.container}>
-            <TextInput
-            underlineColorAndroid="transparent"
-            placeholder="Enter Your Todo"
-            style={styles.input}
-        />
-        <TouchableOpacity 
-            style={{ marginTop: 20, marginLeft: 10, backgroundColor: 'black' }}
-            onPress={() => {}}
-            >
-            <Text style={{ color: 'white', padding: 15 }}>Submit</Text>
-        </TouchableOpacity>
-        </View>  
+        <AddTodo />
     );
   }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-    },
-    input: {
-        marginTop: 20,
-        width: '70%',
-        alignSelf: 'center',
-        borderWidth: 2,
-        borderColor: 'black'
-    }
-});
