1154910a67e31102f933277556d4d6c5f4532c79
Arnaz <arnazmursid@gmail.com>
2017-11-30 17:42:28 +0700

    move the store in app.js and add flex

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { View } from 'react-native';
 import AddTodo from './addTodo';
 import ListTodo from './listTodo';
-import reducer from './reducer';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-const store = createStore(reducer);
 
 export default class Page extends React.Component {
   render() {
     return (
-        <Provider store={store}>
-            <AddTodo />
-            <ListTodo  />
-        </Provider>    
+      <View style={{ flex: 1 }}>
+        <AddTodo />
+        <ListTodo />
+      </View>     
     );
   }
 }
+
+// import React from 'react';
+// import { FlatList, View, StyleSheet } from 'react-native';
+// import { connect } from 'react-redux';
+
+// class ListTodo extends React.Component {
+//   render() {
+//     return (
+//         <View style={styles.container}>
+//             {this.props.todo.map((data, index) => {
+//                 console.log("Data", data);
+//             })}
+//         </View>  
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1
+//     }
+// });
+
+// const mapStateToProps = ({ todos }) => {
+//     todo: todos
+// }
+
+// export default connect(mapStateToProps, null)(ListTodo);
