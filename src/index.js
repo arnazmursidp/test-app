f9774521ecabb6d40e8e4ffb1c5a6fadbb1c98af
Arnaz <arnazmursid@gmail.com>
2017-11-30 13:42:43 +0700

    make store for reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import AddTodo from './addTodo';
+import reducer from './reducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
 
 export default class Page extends React.Component {
   render() {
     return (
-        <AddTodo />
+        <Provider store={store}>
+            <AddTodo />
+        </Provider>    
     );
   }
 }
