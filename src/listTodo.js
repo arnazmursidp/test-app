142795188fc44f828c54098fd4831573b6b76ae9
Arnaz <arnazmursid@gmail.com>
2017-11-30 13:57:29 +0700

    import and make view

---
+++ src/listTodo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ListView, View } from 'react-native';
+
+export default class AddTodo extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+            
+        </View>  
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    
+});
