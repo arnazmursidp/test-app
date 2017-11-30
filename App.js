e94dc3c5f44696175c2773373f980a6bd3fc059b
Arnaz <arnazmursid@gmail.com>
2017-11-30 11:35:09 +0700

    change import name

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import reducer from './src/reducers';
+import Page from './src/index';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Text>Test</Text>
+      <Page />
     );
   }
 }
